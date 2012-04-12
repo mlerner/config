121c2020751049e60d6de68439938f4449181fb7
Corey Farwell <coreyf@rwell.org>
2012-04-12 14:53:09 -0700

    put everything in a __name__ conditional

--- deploy.py
+++ deploy.py
@@ -10,23 +10,24 @@ def is_config(f):
         return False
     return True
 
-files = filter(is_config, os.listdir("."))
-backup = "/tmp/backup-%d" % time.time()
-os.mkdir(backup)
-backed_up = set()
+if __name__ == "__main__":
+    files = filter(is_config, os.listdir("."))
+    backup = "/tmp/backup-%d" % time.time()
+    os.mkdir(backup)
+    backed_up = set()
 
-for f in files:
-    src = os.path.join(os.getcwd(), f)
-    dest = os.path.join(os.getenv("HOME"), "."+f)
-    if os.path.islink(dest):
-        os.remove(dest) 
-    elif os.path.exists(dest):
-        shutil.move(dest, backup)
-        backed_up.add(dest)
-    os.symlink(src, dest)
+    for f in files:
+        src = os.path.join(os.getcwd(), f)
+        dest = os.path.join(os.getenv("HOME"), "."+f)
+        if os.path.islink(dest):
+            os.remove(dest) 
+        elif os.path.exists(dest):
+            shutil.move(dest, backup)
+            backed_up.add(dest)
+        os.symlink(src, dest)
 
-if len(backed_up) > 0:
-    print("The following files have been backed up to " + backup + ": ")
-    print(" ".join(map(os.path.basename, backed_up)))
+    if len(backed_up) > 0:
+        print("The following files have been backed up to " + backup + ": ")
+        print(" ".join(map(os.path.basename, backed_up)))
 
-#os.execlp("git", ("submodule --init update")) # don't want it to replace process though
+    #os.execlp("git", ("submodule --init update")) # don't want it to replace process though
